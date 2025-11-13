b1abd22a78a1240b85a8cc2e5e06c6d6236f3494
RichardSoaresPoltrononieri <tecnicorichard1@gmail.com>
2025-11-13 17:28:57 -0300

    Initial commit

--- src/config/creds.js
+++ src/config/creds.js
@@ -2,7 +2,7 @@ module.exports = {
     AwsConfig: {
         region: "us-east-1",
         endpoint: "https://dynamodb.us-east-1.amazonaws.com",
-        accessKeyId: "AKIA5BQP75V6UJ6BLP7Y",
-        secretAccessKey: "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"
+        accessKeyId: "SUA CHAVE DE ACESSO AQUI",
+        secretAccessKey: "SUA CHAVE SECRETA AQUI"
     }
 };
