8485c6211b5c20bc97caef7a284f76e71a79f3e2
RichardSoaresPoltrononieri <tecnicorichard1@gmail.com>
2025-11-14 15:54:36 -0300

    Incluindo funções CRUD

--- src/config/creds.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    AwsConfig: {
-        region: "us-east-1",
-        endpoint: "https://dynamodb.us-east-1.amazonaws.com",
-        accessKeyId: "SUA CHAVE AQUI",
-        secretAccessKey: "SUA CHAVE AQUI"
-    }
-};
